c8893306ba54c7094c36f2944b415c244c7ab795
Daniel Cortez <dacortez79@gmail.com>
2013-09-15 18:31:19 -0300

    Implementa leitura do arquivo de entrada.

--- src/dacortez/minaDeOuro/Main.java
+++ src/dacortez/minaDeOuro/Main.java
@@ -3,6 +3,11 @@
  */
 package dacortez.minaDeOuro;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * @author dacortez
  *
@@ -13,7 +18,24 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Ol‡ mundo!");
+		if (args.length < 1) {
+			System.out.println("Nome do arquivo de entrada.");
+			return;
+		}
+		try	{
+			BufferedReader input = new BufferedReader(new FileReader(args[0]));
+		    String line = null;
+		    while ((line = input.readLine()) != null) {
+		    	System.out.println(line);
+		    }
+		    input.close();
+		}
+		catch (FileNotFoundException ex) {
+			System.err.println("Arquivo de entrada '" + args[0] + "' n‹o encontrado.");
+		}	
+		catch (IOException ex) {
+			System.err.println("Erro na leitura do arquivo de entrada: " + ex.getMessage());
+		}
 	}
 
 }
