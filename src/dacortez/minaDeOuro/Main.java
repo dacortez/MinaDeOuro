9421da6038d58ee9639a2b471f359125baf6e3e3
Daniel Cortez <dacortez79@gmail.com>
2013-09-15 17:52:26 -0300

    Commit inicial.

---
+++ src/dacortez/minaDeOuro/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package dacortez.minaDeOuro;
+
+/**
+ * @author dacortez
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Ol‡ mundo!");
+	}
+
+}
